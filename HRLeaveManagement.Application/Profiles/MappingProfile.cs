623838aa541a79f09caa8785c823eb00f06b30ff
Leon <soetsutakiwasa@hotmail.com>
2023-02-01 12:35:18 +0100

    [MVP0003] Added mapping profiles for the newly added DTO's

--- HRLeaveManagement.Application/Profiles/MappingProfile.cs
+++ HRLeaveManagement.Application/Profiles/MappingProfile.cs
@@ -17,8 +17,17 @@ namespace HRLeaveManagement.Application.Profiles
         {
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
-            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveRequest, ChangeLeaveRequestApprovalDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, UpdateLeaveRequestDto>().ReverseMap();
         }
     }
 }
