9fe213133cb01ac99e1a2c943a2b3833449715ec
Leon <soetsutakiwasa@hotmail.com>
2023-02-06 12:38:25 +0100

    [MVP0008] Added and configured application api, and created first migration

--- HRLeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
+++ HRLeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -13,7 +13,7 @@ namespace HRLeaveManagement.Infrastructure
 {
     public static class InfrastructureServicesRegistration
     {
-        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
             services.AddTransient<IEmailSender, EmailSender>();
--- HRLeaveManagement.Persistence/PersistenceServicesRegistration.cs
+++ HRLeaveManagement.Persistence/PersistenceServicesRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using HRLeaveManagement.Application.Contracts.Persistence;
 using HRLeaveManagement.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -14,15 +15,33 @@ namespace HRLeaveManagement.Persistence
 {
     public static class PersistenceServicesRegistration
     {
-        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<LeaveManagementDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("LeaveManagementConnectionString")));
+            services.AddDbContext<LeaveManagementDbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("LeaveManagementConnectionString"))
+            );
 
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
             services.AddScoped<LeaveAllocationRepository, LeaveAllocationRepository>();
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
             return services;
+        }
+    }
+
+    public class LeaveManagementDbContextFactory : IDesignTimeDbContextFactory<LeaveManagementDbContext>
+    {
+        public LeaveManagementDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<LeaveManagementDbContext>();
+            var connectionString = configuration.GetConnectionString("LeaveManagementConnectionString");
+            builder.UseSqlServer(connectionString);
+            return new LeaveManagementDbContext(builder.Options);
         }
     }
 }
