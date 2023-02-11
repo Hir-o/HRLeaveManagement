da144d16908e9a8faf596e07f22259e926561b68
Leon <soetsutakiwasa@hotmail.com>
2023-02-11 13:40:13 +0100

    [MVP0016] Setup custom API clients and base code

--- HR.LeaveManagement.MVC/Program.cs
+++ HR.LeaveManagement.MVC/Program.cs
@@ -1,8 +1,16 @@
+using HR.LeaveManagement.MVC.Contracts;
 using HR.LeaveManagement.MVC.Services;
+using HR.LeaveManagement.MVC.Services.Base;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient<IClient, Client>(cl => cl.BaseAddress = new Uri("https://localhost:44378"));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+//builder.Services.AddScoped<ILeaveTypeService, LeaveTypeService>();
+//builder.Services.AddScoped<ILeaveRequestService, LeaveRequestService>();
+//builder.Services.AddScoped<ILeaveAllocationService, LeaveAllocationService>();
+builder.Services.AddSingleton<ILocalStorageService, LocalStorageService>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
---
+++ HR.LeaveManagement.MVC/Services/Base/BaseHttpService.cs
@@ -0,0 +1,34 @@
+﻿using HR.LeaveManagement.MVC.Contracts;
+using System.Net.Http.Headers;
+
+namespace HR.LeaveManagement.MVC.Services.Base
+{
+    public class BaseHttpService
+    {
+        protected readonly ILocalStorageService _localStorage;
+        protected readonly IClient _client;
+
+        public BaseHttpService(ILocalStorageService localStorage, IClient client)
+        {
+            _localStorage = localStorage;
+            _client = client;
+        }
+
+        protected Response<Guid> ConvertApiExceptions<Guid>(ApiException ex)
+        {
+            if (ex.StatusCode == 400)
+                return new Response<Guid>() { Message = "Validation errors have occured.", ValidationErrors = ex.Response, Success = false };
+            else if (ex.StatusCode == 404)
+                return new Response<Guid>() { Message = "The requested item could not be found.", Success = false };
+            else
+                return new Response<Guid>() { Message = "Something went wrong, please try again.", Success = false };
+        }
+
+        protected void AddBearerToken()
+        {
+            if (_localStorage.Exists("token"))
+                _client.HttpClient.DefaultRequestHeaders.Authorization = 
+                    new AuthenticationHeaderValue("Bearer", _localStorage.GetStorageValue<string>("token"));
+        }
+    }
+}
